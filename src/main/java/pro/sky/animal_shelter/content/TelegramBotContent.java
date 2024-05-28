5d0eda1c549ca49c13067cf28321ce2268b61838
Владимир <100162794+Vladimir-Korolev555@users.noreply.github.com>
2024-05-28 23:35:01 +1100

    I added the necessary dependencies and made changes to the versions to make the bot work correctly, created several packages and classes to interact with the bot, taught it to handle the start command

---
+++ src/main/java/pro/sky/animal_shelter/content/TelegramBotContent.java
@@ -0,0 +1,6 @@
+package pro.sky.animal_shelter.content;
+
+public class TelegramBotContent {
+
+    public static final String WELCOME_MESSAGE = "Добро пожаловать в нашего бота! Введите /help, чтобы узнать, что я могу.";
+}
