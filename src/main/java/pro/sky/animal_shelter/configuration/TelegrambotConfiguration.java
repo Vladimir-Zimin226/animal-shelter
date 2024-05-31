c81d4bbabbe7faf362a16a47e211f8394ee66829
origin <tuffx4nn@gmail.com>
2024-05-31 17:56:12 +0500

    some buttons were done, but a lot of things are misunderstood

--- src/main/java/pro/sky/animal_shelter/configuration/TelegrambotConfiguration.java
+++ src/main/java/pro/sky/animal_shelter/configuration/TelegrambotConfiguration.java
@@ -1,6 +1,5 @@
 package pro.sky.animal_shelter.configuration;
 
-
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,17 +7,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class TelegrambotConfiguration {
-
+public class TelegramBotConfiguration {
 
     @Value("${telegram.bot.token}")
     private String token;
 
     @Bean
     public TelegramBot telegramBot() {
-        TelegramBot telegramBot = new TelegramBot(token);
-        telegramBot.execute(new DeleteMyCommands());
-        return telegramBot;
+        TelegramBot bot = new TelegramBot(token);
+        bot.execute(new DeleteMyCommands());
+        return bot;
     }
-
 }
--- src/main/java/pro/sky/animal_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter/listener/TelegramBotUpdatesListener.java
@@ -1,5 +1,7 @@
 package pro.sky.animal_shelter.listener;
 
+import com.pengrad.telegrambot.model.request.KeyboardButton;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
@@ -11,6 +13,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static pro.sky.animal_shelter.content.TelegramBotContent.*;
@@ -35,12 +39,38 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
             if (update.message() != null && update.message().text() != null) {
                 String chatId = update.message().chat().id().toString();
                 String text = update.message().text();
-                if (text.equals("/start")) {
-                    SendMessage message = new SendMessage(chatId, WELCOME_MESSAGE);
-                    SendResponse response = telegramBot.execute(message);
+                switch (text) {
+                    case "/start":
+                        SendMessage welcomeMessage = new SendMessage(chatId,WELCOME_MESSAGE);
+                        welcomeMessage.replyMarkup(createKeyboard());
+                        SendResponse welcomeMessageResponse = telegramBot.execute(welcomeMessage);
+                        break;
+                    case "История приюта":
+                        SendMessage shelterHistory = new SendMessage(chatId,SHELTER_HISTORY);
+                        shelterHistory.replyMarkup(createKeyboard());
+                        SendResponse shelterHistoryResponse = telegramBot.execute(shelterHistory);
                 }
+
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
+    }
+    private ReplyKeyboardMarkup createKeyboard() {
+        KeyboardButton button1 = new KeyboardButton("История приюта");
+        KeyboardButton button2 = new KeyboardButton("Расписание работы");
+        KeyboardButton button3 = new KeyboardButton("Схема проезда");
+        KeyboardButton button4 = new KeyboardButton("Оформить пропуск");
+        KeyboardButton button5 = new KeyboardButton("Техника безопасности");
+        KeyboardButton button6 = new KeyboardButton("Оставить контакты для связи");
+        KeyboardButton button7 = new KeyboardButton("Назад");
+
+        KeyboardButton[][] keyboardButtons = {
+                {button1, button2},
+                {button3, button4},
+                {button5, button6},
+                {button7}
+        };
+
+        return new ReplyKeyboardMarkup(keyboardButtons).resizeKeyboard(true).oneTimeKeyboard(true);
     }
 }
