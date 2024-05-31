04e6f24da2420229bf488a22034bc8065a887aec
origin <tuffx4nn@gmail.com>
2024-05-31 16:16:59 +0500

    there is some mistake, i didnt get what it is

--- src/main/java/pro/sky/animal_shelter/process/TelegrambotProcess.java
+++ src/main/java/pro/sky/animal_shelter/process/TelegrambotProcess.java
@@ -1,19 +1,49 @@
 package pro.sky.animal_shelter.process;
 
+import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TelegrambotProcess implements UpdatesListener {
 
 
+    private Logger logger = LoggerFactory.getLogger(TelegrambotProcess.class);
 
+    @Autowired
+    private TelegramBot telegramBot;
+
+    @PostConstruct
+    public void init() {
+        telegramBot.setUpdatesListener(this);
+    }
 
 
 
     @Override
-    public int process(List<Update> list) {
-        return 0;
+    public int process(List<Update> updates) {
+        updates.forEach(update -> {
+            logger.info("Processing update: {}", update);
+            if (update.message() != null && update.message().text() != null) {
+                Long chatId = update.message().chat().id();
+                String messageGot = update.message().text();
+                if (messageGot.equals("/start")) {
+                    SendMessage message = new SendMessage(chatId, "Здравствуйте, я бот - помощник, направленный на помощь в получении питомца. Что вас интересует?");
+                    SendResponse response = telegramBot.execute(message);
+                }
+            }
+        });
+        return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 }
