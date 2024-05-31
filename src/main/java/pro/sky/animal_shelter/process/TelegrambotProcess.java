814e37c2df7ae8cf8dc0dfdfbe19bfc798e32252
origin <tuffx4nn@gmail.com>
2024-05-31 15:42:36 +0500

    stated with adding application.properties, configuration.java, got bot_toke from bot-father and added it to project. Created TelegrambotProcess.java

---
+++ src/main/java/pro/sky/animal_shelter/process/TelegrambotProcess.java
@@ -0,0 +1,19 @@
+package pro.sky.animal_shelter.process;
+
+import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Update;
+
+import java.util.List;
+
+public class TelegrambotProcess implements UpdatesListener {
+
+
+
+
+
+
+    @Override
+    public int process(List<Update> list) {
+        return 0;
+    }
+}
