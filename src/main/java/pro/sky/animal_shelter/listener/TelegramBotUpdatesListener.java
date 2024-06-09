8c14df71d4c6ef08ab8972d54819b6ef05e62015
origin <tuffx4nn@gmail.com>
2024-06-09 15:23:31 +0500

    отрефакторил код, добавил грамотную обработку данных при вводе номера телефона.

--- src/main/java/pro/sky/animal_shelter/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/animal_shelter/listener/TelegramBotUpdatesListener.java
@@ -5,38 +5,43 @@ import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import com.pengrad.telegrambot.request.SendPhoto;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import org.springframework.stereotype.Service;
 import pro.sky.animal_shelter.chatStates.ChatStateForBackButton;
 import pro.sky.animal_shelter.chatStates.ChatStateForContactInfo;
 import pro.sky.animal_shelter.entity.Users;
 import pro.sky.animal_shelter.service.services.UserService;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import static pro.sky.animal_shelter.content.TelegramBotContent.*;
 
+/**
+ * Service to handle Telegram Bot updates.
+ */
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
     private final UserService userService;
-
-    public TelegramBotUpdatesListener(UserService userService) {
-        this.userService = userService;
-    }
-
-    Map<String, ChatStateForBackButton> chatStateForBackButtonMap = new HashMap<>();
-    Map<String, ChatStateForContactInfo> chatStateForContactInfoMap = new HashMap<>();
-    Map<String, Users> userContactMap = new HashMap<>();
 
     @Autowired
     private TelegramBot telegramBot;
+
+    private final Map<String, ChatStateForBackButton> chatStateForBackButtonMap = new HashMap<>();
+    private final Map<String, ChatStateForContactInfo> chatStateForContactInfoMap = new HashMap<>();
+    private final Map<String, Users> userContactMap = new HashMap<>();
+
+    @Autowired
+    public TelegramBotUpdatesListener(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostConstruct
     public void init() {
@@ -51,134 +56,221 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                 String chatId = String.valueOf(update.message().chat().id());
                 String text = update.message().text();
                 String telegramId = String.valueOf(update.message().from().id());
-                ChatStateForBackButton chatStateForBackButton;
-                if (text.equals("/start")) {
-                    SendPhoto welcomePhoto = new SendPhoto(chatId, WELCOME_PHOTO);
-                    telegramBot.execute(welcomePhoto);
-                    SendMessage welcomeMessage = new SendMessage(chatId, WELCOME_MESSAGE);
-                    telegramBot.execute(welcomeMessage);
-                } else if (text.equals("/help")) {
-                    SendMessage whatUWant = new SendMessage(chatId, "Я являюсь ботом приюта Счастье В Дом. Нажмите на кнопку ниже, чтобы узнать о нашей компании больше");
-                    whatUWant.replyMarkup(createKeyBoardForStart());
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("main_menu", "null"));
-                    telegramBot.execute(whatUWant);
-                } else if (text.equals("Узнать информацию о приюте")) {
-                    SendMessage whatUWant = new SendMessage(chatId, "Что вы хотели бы узнать?");
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelter_info", "main_menu"));
-                    whatUWant.replyMarkup(createKeyboardForShelterInfo());
-                    telegramBot.execute(whatUWant);
-                } else if (text.equals("История приюта")) {
-                    SendMessage shelterHistoryMessage = new SendMessage(chatId, HISTORY);
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
-                    shelterHistoryMessage.replyMarkup(createBackKeyboard());
-                    telegramBot.execute(shelterHistoryMessage);
-                } else if (text.equals("Расписание работы")) {
-                    SendMessage shelterHistoryMessage = new SendMessage(chatId, OPENING_HOURS);
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
-                    shelterHistoryMessage.replyMarkup(createBackKeyboard());
-                    telegramBot.execute(shelterHistoryMessage);
-                } else if (text.equals("Схема проезда")) {
-                    SendPhoto shelterMapPhoto = new SendPhoto(chatId, SCHEMA_SHELTER);
-                    telegramBot.execute(shelterMapPhoto);
-                    SendMessage shelterHistoryMessage = new SendMessage(chatId, ADDRES);
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
-                    shelterHistoryMessage.replyMarkup(createBackKeyboard());
-                    telegramBot.execute(shelterHistoryMessage);
-                } else if (text.equals("Помочь приюту")) {
-                    SendMessage shelterHistoryMessage = new SendMessage(chatId, DONATE);
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
-                    shelterHistoryMessage.replyMarkup(createBackKeyboard());
-                    telegramBot.execute(shelterHistoryMessage);
-                } else if (text.equals("Техника безопасности")) {
-                    SendMessage techdefMessage = new SendMessage(chatId, SAFETY_MEASURES);
-                    chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
-                    techdefMessage.replyMarkup(createBackKeyboard());
-                    telegramBot.execute(techdefMessage);
-                } else if (text.equals("Назад") && chatStateForBackButtonMap.containsKey(chatId)) {
-                    chatStateForBackButton = chatStateForBackButtonMap.get(chatId);
-                    if (chatStateForBackButton.getPreviousMenu().equals("main_menu")) {
-                        SendMessage backMessage = new SendMessage(chatId, "Возвращаемся");
-                        backMessage.replyMarkup(createKeyBoardForStart());
-                        telegramBot.execute(backMessage);
-                        chatStateForBackButton.setCurrentMenu("main_menu", "null");
-                    } else if (chatStateForBackButton.getPreviousMenu().equals("shelter_info")) {
-                        SendMessage backMessage = new SendMessage(chatId, "Возвращаемся");
-                        backMessage.replyMarkup(createKeyboardForShelterInfo());
-                        telegramBot.execute(backMessage);
-                        chatStateForBackButton.setCurrentMenu("shelter_info", "main_menu");
-                    }
-                }else if (text.equals("Оставить контакты для связи")) {
-                    initiatePassProcess(chatId);
-                }else if (text.equals("Согласен(-на)") || !chatStateForContactInfoMap.get(chatId).equals(ChatStateForContactInfo.WAITING_FOR_CONFIRMATION)) {
-                    switch (chatStateForContactInfoMap.getOrDefault(chatId,ChatStateForContactInfo.NONE)) {
-                        case WAITING_FOR_CONFIRMATION:
-                            hanldeConfirmation(chatId);
-                            break;
-                        case WAITING_FOR_FULL_NAME:
-                            handleFullName(chatId, text, telegramId);
-                            break;
-                        case WAITING_FOR_PHONE_NUMBER:
-                            handlePhoneNumber(chatId, text);
-                            break;
-                        default:
-                            sendUnknownCommandMessage(chatId);
-                            break;
-                    }
-                }
-
+                handleUpdate(chatId, text, telegramId);
             }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    private void initiatePassProcess(String chatId) {
-        SendMessage passMessage = new SendMessage(chatId, "Согласны ли вы на обработку некоторых данных?");
-        passMessage.replyMarkup(createKeyboardForContactInfo());
-        telegramBot.execute(passMessage);
+    private void handleUpdate(String chatId, String text, String telegramId) {
+        switch (text) {
+            case "/start":
+                sendWelcomeMessage(chatId);
+                break;
+            case "/help":
+                sendHelpMessage(chatId);
+                break;
+            case "Узнать информацию о приюте":
+                sendShelterInfoMenu(chatId);
+                break;
+            case "История приюта":
+                sendShelterHistory(chatId);
+                break;
+            case "Расписание работы":
+                sendOpeningHours(chatId);
+                break;
+            case "Схема проезда":
+                sendShelterMap(chatId);
+                break;
+            case "Помочь приюту":
+                sendDonationInfo(chatId);
+                break;
+            case "Техника безопасности":
+                sendSafetyMeasures(chatId);
+                break;
+            case "Назад":
+                handleBackButton(chatId);
+                break;
+            case "Оставить контакты для связи":
+                initiateContactInfoProcess(chatId);
+                break;
+            default:
+                handleContactInfoProcess(chatId, text, telegramId);
+                break;
+        }
+    }
+
+    private void sendWelcomeMessage(String chatId) {
+        logger.info("Sending welcome message to chat {}", chatId);
+        telegramBot.execute(new SendPhoto(chatId, WELCOME_PHOTO));
+        telegramBot.execute(new SendMessage(chatId, WELCOME_MESSAGE));
+    }
+
+    private void sendHelpMessage(String chatId) {
+        logger.info("Sending help message to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, "Я являюсь ботом приюта Счастье В Дом. Нажмите на кнопку ниже, чтобы узнать о нашей компании больше");
+        message.replyMarkup(createKeyBoardForStart());
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("main_menu", "null"));
+        telegramBot.execute(message);
+    }
+
+    private void sendShelterInfoMenu(String chatId) {
+        logger.info("Sending shelter info menu to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, "Что вы хотели бы узнать?");
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelter_info", "main_menu"));
+        message.replyMarkup(createKeyboardForShelterInfo());
+        telegramBot.execute(message);
+    }
+
+    private void sendShelterHistory(String chatId) {
+        logger.info("Sending shelter history to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, HISTORY);
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
+        message.replyMarkup(createBackKeyboard());
+        telegramBot.execute(message);
+    }
+
+    private void sendOpeningHours(String chatId) {
+        logger.info("Sending opening hours to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, OPENING_HOURS);
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
+        message.replyMarkup(createBackKeyboard());
+        telegramBot.execute(message);
+    }
+
+    private void sendShelterMap(String chatId) {
+        logger.info("Sending shelter map to chat {}", chatId);
+        telegramBot.execute(new SendPhoto(chatId, SCHEMA_SHELTER));
+        SendMessage message = new SendMessage(chatId, ADDRES);
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
+        message.replyMarkup(createBackKeyboard());
+        telegramBot.execute(message);
+    }
+
+    private void sendDonationInfo(String chatId) {
+        logger.info("Sending donation info to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, DONATE);
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
+        message.replyMarkup(createBackKeyboard());
+        telegramBot.execute(message);
+    }
+
+    private void sendSafetyMeasures(String chatId) {
+        logger.info("Sending safety measures to chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, SAFETY_MEASURES);
+        chatStateForBackButtonMap.put(chatId, new ChatStateForBackButton("shelters", "shelter_info"));
+        message.replyMarkup(createBackKeyboard());
+        telegramBot.execute(message);
+    }
+
+    private void handleBackButton(String chatId) {
+        logger.info("Handling back button for chat {}", chatId);
+        ChatStateForBackButton state = chatStateForBackButtonMap.get(chatId);
+        if (state != null) {
+            switch (state.getPreviousMenu()) {
+                case "main_menu":
+                    sendBackToMainMenu(chatId, state);
+                    break;
+                case "shelter_info":
+                    sendBackToShelterInfo(chatId, state);
+                    break;
+                default:
+                    logger.warn("Unknown previous menu: {}", state.getPreviousMenu());
+                    sendUnknownCommandMessage(chatId);
+                    break;
+            }
+        } else {
+            logger.warn("No state found for chat {}", chatId);
+            sendUnknownCommandMessage(chatId);
+        }
+    }
+
+    private void sendBackToMainMenu(String chatId, ChatStateForBackButton state) {
+        SendMessage message = new SendMessage(chatId, "Возвращаемся");
+        message.replyMarkup(createKeyBoardForStart());
+        telegramBot.execute(message);
+        state.setCurrentMenu("main_menu", "null");
+    }
+
+    private void sendBackToShelterInfo(String chatId, ChatStateForBackButton state) {
+        SendMessage message = new SendMessage(chatId, "Возвращаемся");
+        message.replyMarkup(createKeyboardForShelterInfo());
+        telegramBot.execute(message);
+        state.setCurrentMenu("shelter_info", "main_menu");
+    }
+
+    private void initiateContactInfoProcess(String chatId) {
+        logger.info("Initiating contact info process for chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, "Согласны ли вы на обработку некоторых данных?");
+        message.replyMarkup(createKeyboardForContactInfo());
+        telegramBot.execute(message);
         chatStateForContactInfoMap.put(chatId, ChatStateForContactInfo.WAITING_FOR_CONFIRMATION);
     }
 
-    private void hanldeConfirmation(String chatId) {
-        SendMessage fullNameMessage = new SendMessage(chatId, "Тогда пришлите пожалуйста ФИО");
-        telegramBot.execute(fullNameMessage);
+    private void handleContactInfoProcess(String chatId, String text, String telegramId) {
+        ChatStateForContactInfo state = chatStateForContactInfoMap.getOrDefault(chatId, ChatStateForContactInfo.NONE);
+        switch (state) {
+            case WAITING_FOR_CONFIRMATION:
+                if ("Согласен(-на)".equals(text)) {
+                    handleConfirmation(chatId);
+                }
+                break;
+            case WAITING_FOR_FULL_NAME:
+                handleFullName(chatId, text, telegramId);
+                break;
+            case WAITING_FOR_PHONE_NUMBER:
+                handlePhoneNumber(chatId, text);
+                break;
+            default:
+                sendUnknownCommandMessage(chatId);
+                break;
+        }
+    }
+
+    private void handleConfirmation(String chatId) {
+        logger.info("Handling confirmation for chat {}", chatId);
+        SendMessage message = new SendMessage(chatId, "Тогда пришлите пожалуйста ФИО");
+        telegramBot.execute(message);
         chatStateForContactInfoMap.put(chatId, ChatStateForContactInfo.WAITING_FOR_FULL_NAME);
     }
 
     private void handleFullName(String chatId, String text, String telegramId) {
+        logger.info("Handling full name for chat {}", chatId);
         Users user = new Users();
         user.setName(text);
         user.setTelegramId(telegramId);
         user.setVolunteer(false);
         userContactMap.put(chatId, user);
 
-        SendMessage phoneNumberMessage = new SendMessage(chatId, "Пришлите номер телефона, в формате: +74953500505");
-        telegramBot.execute(phoneNumberMessage);
-
+        SendMessage message = new SendMessage(chatId, "Пришлите номер телефона, в формате: +74953500505");
+        telegramBot.execute(message);
         chatStateForContactInfoMap.put(chatId, ChatStateForContactInfo.WAITING_FOR_PHONE_NUMBER);
-        logger.info("Updated state to WAITING_FOR_PHONE_NUMBER for chat {}", chatId);
     }
 
     private void handlePhoneNumber(String chatId, String text) {
         Users user = userContactMap.get(chatId);
-        if (text.matches("^\\+7\\d{10}$") && user != null) {
+        if (user != null && text.matches("^\\+7\\d{10}$")) {
+            logger.info("Handling phone number for chat {}", chatId);
             user.setPhoneNumber(text);
-            SendMessage confirmationMessage = new SendMessage(chatId, "Благодарим, мы с вами свяжемся.");
-            telegramBot.execute(confirmationMessage);
+            SendMessage message = new SendMessage(chatId, "Благодарим, мы с вами свяжемся.");
+            telegramBot.execute(message);
+            userService.createUser(user);
+            clearContactInfoState(chatId);
         } else {
-            throw new RuntimeException("Введён некорретный номер телефона, попробуйте снова!");
+            logger.warn("Invalid phone number or user not found for chat {}", chatId);
+            SendMessage message = new SendMessage(chatId, "Вы ввели некорректный номер телефона, попробуйте заново.");
+            telegramBot.execute(message);
         }
-
-
-
-
-        userService.createUser(user);
-        chatStateForContactInfoMap.remove(chatId);
-        userContactMap.remove(chatId);
-        logger.info("Removed chat {} from chatStateForContactInfoMap and userContactMap", chatId);
     }
 
+    private void clearContactInfoState(String chatId) {
+        chatStateForContactInfoMap.remove(chatId);
+        userContactMap.remove(chatId);
+        logger.info("Cleared contact info state for chat {}", chatId);
+    }
 
     private void sendUnknownCommandMessage(String chatId) {
+        logger.warn("Unknown command received for chat {}", chatId);
         SendMessage message = new SendMessage(chatId, "Неизвестная команда. Пожалуйста, используйте клавиатуру для выбора опций.");
         telegramBot.execute(message);
     }
@@ -218,8 +310,9 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     private ReplyKeyboardMarkup createKeyboardForContactInfo() {
         KeyboardButton button1 = new KeyboardButton("Согласен(-на)");
         KeyboardButton button2 = new KeyboardButton("Назад");
+
         KeyboardButton[][] keyboardButtons = {
-                {button1, button2},
+                {button1, button2}
         };
 
         return new ReplyKeyboardMarkup(keyboardButtons).resizeKeyboard(true).oneTimeKeyboard(true);
@@ -227,9 +320,7 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private ReplyKeyboardMarkup createBackKeyboard() {
         KeyboardButton button = new KeyboardButton("Назад");
-
         KeyboardButton[] keyboardButtons = {button};
-
         return new ReplyKeyboardMarkup(keyboardButtons).resizeKeyboard(true).oneTimeKeyboard(true);
     }
 }
